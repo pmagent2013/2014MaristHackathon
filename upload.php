61f345ce54aa2b146965b7acb5a1ba5e790df051
kyle170 <kyle@gavalchin.com>
2014-11-22 16:13:18 -0500

    code fix

--- upload.php
+++ upload.php
@@ -1,14 +1,9 @@
 <?php
+sleep(10);
 if(isset($_FILES['file'])) {
-
     if(move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name'])){
-		sleep(10);
                 echo "success";
-
                exit;
-
     }
-
 }
-
 ?>
