0f3dc567e2f7f0f9588d691b4c99ce82bd1a9b8a
kyle170 <kyle@gavalchin.com>
2014-11-22 13:48:53 -0500

    thing

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <title>MaristNotes</title>
     <link rel="stylesheet" href="css/foundation.css" />
     <script src="js/vendor/modernizr.js"></script>
+	   <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
+
+   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+
+   <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
 	<script>
 	function showThumbnails(){
   var files= document.getElementById('file').files;
@@ -96,9 +101,6 @@ function handleFile(files){
 
             
 
-         </script>
-<script>
-
   $(function() {
 
     $("#progressbar").progressbar({
@@ -128,17 +130,6 @@ function handleFile(files){
 		
   </head>
   <body>
-    
-
-  
-
-   
-
-   <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
-
-   <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-
-   <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
 
    <style type="text/css">
 
