11141436e67deb518423d004bf0e3c59e7cc6c0a
kyle170 <kyle@gavalchin.com>
2014-11-22 17:52:39 -0500

    Menu Title

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
  <nav class="top-bar" data-topbar>
         <ul class="title-area">
             <li class="name">
-                <h1><a href="#Home"><img src="img/NavLogo2.png"></a></h1>
+                <h1>Int-Elligence</h1>
             </li>
             <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         </ul>
