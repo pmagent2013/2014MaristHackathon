b79a2083e9d1c8fad5b9410776001f23bb057a11
kyle170 <kyle@gavalchin.com>
2014-11-22 16:11:58 -0500

    Uploader Post Processing

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 if(isset($_FILES['file'])) {
 
     if(move_uploaded_file($_FILES['file']['tmp_name'], "uploads/" . $_FILES['file']['name'])){
-
+		sleep(10);
                 echo "success";
 
                exit;
