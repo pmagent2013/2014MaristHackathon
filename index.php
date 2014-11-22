aa2b0fecf3ff51a9879aca998b4d27566e2f8c8d
kyle170 <kyle@gavalchin.com>
2014-11-22 18:26:16 -0500

    Double Load

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
 	<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css">
    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
    <script src="js/ajax.js"></script>
-   <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
-   <script src="js/ajax.js"></script>
    </head>
    <body>
  <nav class="top-bar" data-topbar>
