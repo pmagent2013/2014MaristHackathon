eee24865ac7f46133771300ede2b2949120c47e9
kyle170 <kyle@gavalchin.com>
2014-11-22 17:32:20 -0500

    3

--- common.php
+++ common.php
@@ -16,6 +16,108 @@ databaseConnect();  //Make an active connection to the database
 //-------------- Check if $_POST variables sent ---------------------
 
 
+function parameter($parameter, $form = '0')
+{
+    if ($form == 0) {
+        if (isset($_GET[$parameter])) {
+            return ($_GET[$parameter]);
+            exit;
+        }
+        if (isset($_POST[$parameter])) {
+            return ($_POST[$parameter]);
+            exit;
+        }
+        if (isset($_COOKIE[$parameter])) {
+            return ($_COOKIE[$parameter]);
+            exit;
+        }
+    } elseif ($form == 1) {
+        if (parameter($parameter) != "") {
+            return decrypt(parameter($parameter));
+        }
+    }
+    return false;
+}
+
+
+function encrypt($text)
+{
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $key = "K";
+    if (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE) {
+        return encodeURIComponent(rawurlencode(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, "|||" . $text . "|||", MCRYPT_MODE_ECB, $iv))));
+    } else {
+        return rawurlencode(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, "|||" . $text . "|||", MCRYPT_MODE_ECB, $iv)));
+    }
+}
+
+
+function websiteLastUpdated()
+{
+    $path = realpath($_SERVER['DOCUMENT_ROOT']);
+    $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+    foreach ($objects as $name => $object) {
+        $filetime[] = filemtime($name);
+    }
+    $filetime = max($filetime);
+    return timeago($filetime);
+}
+
+function decrypt($text)
+{
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    $key = "K";
+    $out = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, base64_decode($text), MCRYPT_MODE_ECB, $iv));
+    return substr($out, 3, last_index_of("|||", $out) - 6);
+}
+
+function timeTotal($ptime)
+{
+    $etime = $ptime;
+    if ($etime < 1) {
+        return '0 seconds';
+    }
+    $a = array( //24 * 60 * 60 * 30 *12   =>  'year',
+        //24 * 60 * 60 * 30       =>  'month',
+        24 * 60 * 60 => 'day',
+        60 * 60 => 'hour',
+        60 => 'minute',
+        1 => 'second'
+    );
+    foreach ($a as $secs => $str) {
+        $d = $etime / $secs;
+        if ($d >= 1) {
+            $r = round($d, 2);
+            return $r . ' ' . $str . ($r > 1 ? 's' : '');
+        }
+    }
+}
+
+function timeago($ptime)
+{
+    $etime = time() - $ptime;
+    if ($etime < 1) {
+        return '0 seconds';
+    }
+    $a = array(24 * 60 * 60 * 30 * 12 => 'year',
+        24 * 60 * 60 * 30 => 'month',
+        24 * 60 * 60 => 'day',
+        60 * 60 => 'hour',
+        60 => 'minute',
+        1 => 'second'
+    );
+    foreach ($a as $secs => $str) {
+        $d = $etime / $secs;
+        if ($d >= 1) {
+            $r = round($d);
+            return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
+        }
+    }
+}
+
+
 function errorDisplay($errorMessage = NULL, $Severity = NULL){ 
 	//This function displays any pre-defined trace errors to be called
 	//Any other errors will be recorded in the apache logs
