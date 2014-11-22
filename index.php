d87df46f6cbd9b6ea4237c159582e681a9398be2
kyle170 <kyle@gavalchin.com>
2014-11-22 17:53:28 -0500

    Color FIx

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
  <nav class="top-bar" data-topbar>
         <ul class="title-area">
             <li class="name">
-                <h1>Int-Elligence</h1>
+                <h1 color="white">Int-Elligence</h1>
             </li>
             <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         </ul>
