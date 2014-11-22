de333236ba7f1c91a7577f6ab6d49163964eaecd
kyle170 <kyle@gavalchin.com>
2014-11-22 13:30:11 -0500

    yiss

--- common.php
+++ common.php
@@ -16,7 +16,6 @@ databaseConnect();  //Make an active connection to the database
 //-------------- Check if $_POST variables sent ---------------------
 
 
-
 function errorDisplay($errorMessage = NULL, $Severity = NULL){ 
 	//This function displays any pre-defined trace errors to be called
 	//Any other errors will be recorded in the apache logs
