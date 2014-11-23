d1b906fee8f5a8b0ede7df519a318a7b5fea3c68
kyle170 <kyle@gavalchin.com>
2014-11-22 19:36:38 -0500

    VAR DUMP

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 if(!$_SESSION['authorized']){
+echo var_dump($_POST);
    if(isset($_POST['Login'])){ // if login form submitted
    echo "YES";
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
