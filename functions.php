9815429532b54be42884c95c4a54a3f9f645fd36
kyle170 <kyle@gavalchin.com>
2014-11-22 20:13:51 -0500

    s

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 print('<pre>'); print_r($_POST); print('</pre>');
 if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
-	   if(mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'count(*)') != 1 || $_POST['login_user'] = ""){
+	   if($_POST['login_user'] == ""){
 			header('Location: index.php#login&error=No+User+Given');
-		}
+	   }
       $pass = $_POST['pass']; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
 		  if ($pass == $Password) { //error, wrong password
