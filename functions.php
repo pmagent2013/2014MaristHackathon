bd571e782fe781d97763efd956257738ee453b3b
kyle170 <kyle@gavalchin.com>
2014-11-22 20:41:15 -0500

    sdfsd

--- functions.php
+++ functions.php
@@ -4,13 +4,16 @@ if(!$_SESSION['authorized']){
 	   if($_POST['login_user'] == ""){
 			header('Location: index.php#login&error=No+User+Given');
 	   }
+	   if($_POST['pass'] == ""){
+			header('Location: index.php#login&error=No+Pass');
+	   }
       $pass = $_POST['pass']; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
 		  if ($pass != $Password) { //error, wrong password
 			header('Location: index.php#login&error=Wrong+Password+or+Username');
 		  }else{ 
 			$_SESSION['authorized'] = TRUE;
-			 header('Location: index.php#Home');  
+			header('Location: index.php#Home');  
 		  }
    }
 }
