9aca799e350be111e3fc263a846502618666017f
kyle170 <kyle@gavalchin.com>
2014-11-22 20:02:16 -0500

    FAKLING  --

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 print('<pre>'); print_r($_POST); print('</pre>');
 if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
-	   if(mysql_result(mysql_query("SELECT COUNT(*)  FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'count(*)') == "0"){
+	   if(mysql_result(mysql_query("SELECT COUNT(*)  FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'count(*)') == 0){
 			header('Location: index.php#login&error=No+User+Given');
 		}
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
