c78485ae89747a74bf624ed506e6e4c248cfffb5
kyle170 <kyle@gavalchin.com>
2014-11-22 20:10:36 -0500

    match

--- functions.php
+++ functions.php
@@ -2,16 +2,16 @@
 print('<pre>'); print_r($_POST); print('</pre>');
 if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
-	   if(mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'count(*)') !== 1){
+	   if(mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'count(*)') != 1){
 			header('Location: index.php#login&error=No+User+Given');
 		}
       $pass = $_POST['pass']; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-		  if ($pass !== $Password) { //error, wrong password
-			 header('Location: index.php#login&error=Wrong+Password+or+Username');   
+		  if ($pass == $Password) { //error, wrong password
+			 $_SESSION['authorized'] = TRUE;
+			 header('Location: index.php#Home');  
 		  }else{
-			$_SESSION['authorized'] = TRUE;
-			//header('Location: index.php#Home');
+			header('Location: index.php#login&error=Wrong+Password+or+Username');   
 		  }
    }
 }
