f143f03c865295af02016057c7146508e015c8e0
kyle170 <kyle@gavalchin.com>
2014-11-23 08:38:23 -0500

    formatting

--- common.php
+++ common.php
@@ -160,28 +160,32 @@ function readImageForText($filename){
 	return $response;
 }
 
-function filesize2bytes($str) { 
-    $bytes = 0; 
-
-    $bytes_array = array( 
-        'B' => 1, 
-        'KB' => 1024, 
-        'MB' => 1024 * 1024, 
-        'GB' => 1024 * 1024 * 1024, 
-        'TB' => 1024 * 1024 * 1024 * 1024, 
-        'PB' => 1024 * 1024 * 1024 * 1024 * 1024, 
-    ); 
-
-    $bytes = floatval($str); 
-
-    if (preg_match('#([KMGTP]?B)$#si', $str, $matches) && !empty($bytes_array[$matches[1]])) { 
-        $bytes *= $bytes_array[$matches[1]]; 
-    } 
-
-    $bytes = intval(round($bytes, 2)); 
-
-    return $bytes; 
-} 
+function byteFormat($bytes, $unit = "", $decimals = 2) {
+	$units = array('B' => 0, 'KB' => 1, 'MB' => 2, 'GB' => 3, 'TB' => 4, 
+			'PB' => 5, 'EB' => 6, 'ZB' => 7, 'YB' => 8);
+ 
+	$value = 0;
+	if ($bytes > 0) {
+		// Generate automatic prefix by bytes 
+		// If wrong prefix given
+		if (!array_key_exists($unit, $units)) {
+			$pow = floor(log($bytes)/log(1024));
+			$unit = array_search($pow, $units);
+		}
+ 
+		// Calculate byte value by prefix
+		$value = ($bytes/pow(1024,floor($units[$unit])));
+	}
+ 
+	// If decimals is not numeric or decimals is less than 0 
+	// then set default value
+	if (!is_numeric($decimals) || $decimals < 0) {
+		$decimals = 2;
+	}
+ 
+	// Format output
+	return sprintf('%.' . $decimals . 'f '.$unit, $value);
+  }
 
 
 
