95a2160f194d8d4d0cfb33d1c5f2d8d673e5e36a
kyle170 <kyle@gavalchin.com>
2014-11-22 20:47:26 -0500

    final!

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ if(!$_SESSION['authorized']){
 	   if($_POST['pass'] !== ""){
 			header('Location: index.php#login&error=No+Pass');
 	   }
-	   if(mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'COUNT(*)') == 1){
+	   if(mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'COUNT(*)') !== 1){
 			header('Location: index.php#login&error=User+Does+Not+Exist');
 		}
       $pass = $_POST['pass']; //sets password variable
