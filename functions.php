b4a6fc2b6885d8f85594ecd06e4f785533970792
kyle170 <kyle@gavalchin.com>
2014-11-22 20:56:02 -0500

    asdf

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
-	   if($_POST['login_user'] != ""){
-			//header('Location: index.php#login&error=No+User+Given');
+	   if($_POST['login_user'] !== ""){
+			header('Location: index.php#login&error=No+User+Given');
 	   }
-	   if($_POST['pass'] != ""){
-			//header('Location: index.php#login&error=No+Pass');
+	   if($_POST['pass'] !== ""){
+			header('Location: index.php#login&error=No+Pass');
 	   }
 	   $numRecords = mysql_result(mysql_query("SELECT COUNT(*) FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'COUNT(*)');
 	   if($numRecords == "0"){
@@ -13,8 +13,8 @@ if(!$_SESSION['authorized']){
 		}
       $pass = $_POST['pass']; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-		  if ($pass != $Password) { //error, wrong password
-			//header('Location: index.php#login&error=Wrong+Password+or+Username');
+		  if ($pass !== $Password) { //error, wrong password
+			header('Location: index.php#login&error=Wrong+Password+or+Username');
 		  }else{ 
 			$_SESSION['authorized'] = TRUE;
 			//header('Location: index.php#Home');  
