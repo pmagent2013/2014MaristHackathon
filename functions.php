419c1ff47341ef2fa7b9e0f8b6f619da6c16a918
kyle170 <kyle@gavalchin.com>
2014-11-22 19:41:06 -0500

    Fix Issues

--- functions.php
+++ functions.php
@@ -2,15 +2,14 @@
 print('<pre>'); print_r($_POST); print('</pre>');
 if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
-   echo "YES";
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
-      
-	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['user']."'"), 0, 'password');
-      if (md5($pass) != $Password) { //error, wrong password
+	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
+      if ($pass != $Password) { //error, wrong password
          header('Location: index.php/#login&error=Wrong+Password');   
-      }
-	  $_SESSION['authorized']=TRUE;
-	  header('Location: index.php');
+      }else{
+		$_SESSION['authorized'] = TRUE;
+		header('Location: index.php');
+	  }
    }
 }
 
