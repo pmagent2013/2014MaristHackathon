3c717b907732171bb396917f7edfe9b944a761b3
kyle170 <kyle@gavalchin.com>
2014-11-22 19:53:41 -0500

    blank user fix

--- functions.php
+++ functions.php
@@ -3,8 +3,11 @@ if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-      if ($pass != $Password && $_POST['login_user'] != "") { //error, wrong password
-         header('Location: index.php#login&error=Wrong+Password');   
+	  if($_POST['login_user'] != ""){
+		header('Location: index.php#login&error=No+User+Given');
+	  }
+      if ($pass != $Password) { //error, wrong password
+         header('Location: index.php#login&error=Wrong+Password+or+Username');   
       }else{
 		$_SESSION['authorized'] = TRUE;
 		header('Location: index.php#Home');
