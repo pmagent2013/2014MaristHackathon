b72ed5d9cdff93e31e84938e9a8f81a1887c0811
kyle170 <kyle@gavalchin.com>
2014-11-22 23:51:19 -0500

    sdafasdfasdf

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <html class="no-js" lang="en">
   <head>
     <meta charset="utf-8" />
-	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Int-Elligence</title>
     <link rel="stylesheet" href="css/foundation.css" />
