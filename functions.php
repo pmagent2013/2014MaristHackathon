ba64bdf8d960b54bbbdc2755ab459d9e50b394fc
kyle170 <kyle@gavalchin.com>
2014-11-22 19:54:18 -0500

    FAK!

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-	  if($_POST['login_user'] != ""){
+	  if($_POST['login_user'] == ""){
 		header('Location: index.php#login&error=No+User+Given');
 	  }
       if ($pass != $Password) { //error, wrong password
