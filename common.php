62e2504b9b20e6d8b2ae0f0d078afd31baeba8bd
kyle170 <kyle@gavalchin.com>
2014-11-22 19:37:12 -0500

    Reenable Errors

--- common.php
+++ common.php
@@ -1,5 +1,6 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE);
+//error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL);
 ini_set('display_errors', '1');
 $execution_time = microtime();
 session_start();
