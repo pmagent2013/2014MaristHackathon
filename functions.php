bd934b51a7c55c2d971263035e955933c401457d
kyle170 <kyle@gavalchin.com>
2014-11-22 19:50:45 -0500

    no user login fix

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ if(!$_SESSION['authorized']){
    if(isset($_POST['Login'])){ // if login form submitted
       $pass = isset($_POST['pass']) ? $_POST['pass'] : ''; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-      if ($pass != $Password) { //error, wrong password
+      if ($pass != $Password && $_POST['login_user'] != "") { //error, wrong password
          header('Location: index.php#login&error=Wrong+Password');   
       }else{
 		$_SESSION['authorized'] = TRUE;
