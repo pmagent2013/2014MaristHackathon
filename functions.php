0966c926a053bf2c9ee1ecf6c7d0645ee559bf0c
kyle170 <kyle@gavalchin.com>
2014-11-22 20:57:21 -0500

    yiss

--- functions.php
+++ functions.php
@@ -13,11 +13,11 @@ if(!$_SESSION['authorized']){
 		}
       $pass = $_POST['pass']; //sets password variable
 	  $Password = mysql_result(mysql_query("SELECT `password` FROM `users` WHERE `username` = '".$_POST['login_user']."'"), 0, 'password');
-		  if ($pass !== $Password) { //error, wrong password
-			header('Location: index.php#login&error=Wrong+Password+or+Username');
-		  }else{ 
+		  if ($pass === $Password) { //error, wrong password
 			$_SESSION['authorized'] = TRUE;
-			//header('Location: index.php#Home');  
+			header('Location: index.php#Home');
+		  }else{ 
+			header('Location: index.php#login&error=Wrong+Password+or+Username');
 		  }
    }
 }
